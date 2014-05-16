e7f4f4d2d6b53b08c5ae68e7553ed2b7b33636cb
darthdie <briarbear7@gmail.com>
2014-05-16 16:43:12 -0500

    Added support for a bonus icon. (e.g. Nightmists spell power)

--- Source/src/uk/co/fostorial/sotm/structure/HeroCard.java
+++ Source/src/uk/co/fostorial/sotm/structure/HeroCard.java
@@ -5,239 +5,258 @@ import java.awt.Font;
 import java.io.File;
 
 public class HeroCard extends Card {
-	
-	private String quoteString1;
-	private String quoteString2;
-	private String issueString;
-	
-	private boolean healthPointsVisible;
-	private String healthPointsImage;
-	
-	private String cardText;
-	
-	private Color nameColor;
-	private Color classColor;
-	
-	private Font nameFont;
-	private Color nameFontColor;
-	private Font hpFont;
-	private Color hpFontColor;
-	private Font classFont;
-	private Color classFontColor;
-	private Font descriptionFont;
-	private Color descriptionFontColor;
-	private Font quoteFont;
-	private Color quoteFontColor;
 
-	public HeroCard(String name, Integer id) {
-		super(Card.HERO_CARD, id);
-		setName(name);
-		setNumberInDeck(new Integer(1));
-		setClasses("None");
-		setHealthPoints("N/A");
-		setPortraitFile("images" + File.separator + "herocard" + File.separator + "portrait.png");
-		
-		this.setHealthPointsVisible(false);
-		this.setHealthPointsImage("images" + File.separator + "herocard" + File.separator + "hpimage.png");
-		
-		nameColor = new Color(241,157,15);
-		classColor = new Color(241,157,15);
-		
-		cardText = "Card Text";
-		
-		quoteString1 = "\"Once in while you just";
-		quoteString2 = "gotta improvise!\"";
-		issueString = "- Citizen Snips, The Crab Cometh #4";
-		
-		nameFontColor = Color.white;
-		nameFont = new Font("SF Ferretopia", Font.PLAIN, 50);
-		hpFontColor = Color.black;
-		hpFont = new Font("SF Ferretopia", Font.PLAIN, 70);
-		classFontColor = Color.black;
-		classFont = new Font("Comic Book", Font.PLAIN, 30);
-		descriptionFontColor = Color.black;
-		descriptionFont = new Font("Comic Book", Font.PLAIN, 24);
-		quoteFontColor = Color.black;
-		quoteFont = new Font("Comic Book", Font.PLAIN, 20);
-	}
+    private String quoteString1;
+    private String quoteString2;
+    private String issueString;
 
-	public String getXML()
-	{
-		String xml = "";
-		xml += " <herocard>\n";
-		xml += "  <id>" + getCardID().intValue() + "</id>\n";
-		xml += "  <name>" + getName() + "</name>\n";
-		xml += "  <classes>" + getClasses() + "</classes>\n";
-		xml += "  <healthpoints>" + getHealthPoints() + "</healthpoints>\n";
-		xml += "  <healthpointsvisible>" + isHealthPointsVisible() + "</healthpointsvisible>\n";
-		xml += "  <healthpointsimage>" + getHealthPointsImage() + "</healthpointsimage>\n";
-		xml += "  <portrait>" + getPortraitFile() + "</portrait>\n";
-		xml += "  <numberindeck>" + getNumberInDeck() + "</numberindeck>\n";
-		xml += "  <cardtext>" + getCardText() + "</cardtext>\n";
-		xml += "  <quotestring1>" + getQuoteString1() + "</quotestring1>\n";
-		xml += "  <quotestring2>" + getQuoteString2() + "</quotestring2>\n";
-		xml += "  <issuestring>" + getIssueString() + "</issuestring>\n";
-		xml += "  <classcolour>" + getClassColor().getRGB() + "</classcolour>\n";
-		xml += "  <namecolour>" + getNameColor().getRGB() + "</namecolour>\n";
-		
-		xml += "  <namefontcolor>" + getNameFontColor().getRGB() + "</namefontcolor>\n";
-		xml += "  <namefont>" + getNameFont().getFontName() + ";" + getNameFont().getStyle() + ";" + getNameFont().getSize() + "</namefont>\n";
-		xml += "  <hpfontcolor>" + getHpFontColor().getRGB() + "</hpfontcolor>\n";
-		xml += "  <hpfont>" + getHpFont().getFontName() + ";" + getHpFont().getStyle() + ";" + getHpFont().getSize() + "</hpfont>\n";
-		xml += "  <classfontcolor>" + getClassFontColor().getRGB() + "</classfontcolor>\n";
-		xml += "  <classfont>" + getClassFont().getFontName() + ";" + getClassFont().getStyle() + ";" + getClassFont().getSize() + "</classfont>\n";
-		xml += "  <descriptionfontcolor>" + getDescriptionFontColor().getRGB() + "</descriptionfontcolor>\n";
-		xml += "  <descriptionfont>" + getDescriptionFont().getFontName() + ";" + getDescriptionFont().getStyle() + ";" + getDescriptionFont().getSize() + "</descriptionfont>\n";
-		xml += "  <quotefontcolor>" + getQuoteFontColor().getRGB() + "</quotefontcolor>\n";
-		xml += "  <quotefont>" + getQuoteFont().getFontName() + ";" + getQuoteFont().getStyle() + ";" + getQuoteFont().getSize() + "</quotefont>\n";
-		
-		xml += " </herocard>\n";
-		return xml;
-	}
+    private boolean healthPointsVisible;
+    private String healthPointsImage;
+    
+    private boolean bonusIconVisible;
+    private String bonusIconImage;
 
-	public String getQuoteString1() {
-		return quoteString1;
-	}
+    private String cardText;
 
-	public void setQuoteString1(String quoteString1) {
-		this.quoteString1 = quoteString1;
-	}
+    private Color nameColor;
+    private Color classColor;
 
-	public String getQuoteString2() {
-		return quoteString2;
-	}
+    private Font nameFont;
+    private Color nameFontColor;
+    private Font hpFont;
+    private Color hpFontColor;
+    private Font classFont;
+    private Color classFontColor;
+    private Font descriptionFont;
+    private Color descriptionFontColor;
+    private Font quoteFont;
+    private Color quoteFontColor;
 
-	public void setQuoteString2(String quoteString2) {
-		this.quoteString2 = quoteString2;
-	}
+    public HeroCard(String name, Integer id) {
+        super(Card.HERO_CARD, id);
+        setName(name);
+        setNumberInDeck(1);
+        setClasses("None");
+        setHealthPoints("N/A");
+        setPortraitFile("images" + File.separator + "herocard" + File.separator + "portrait.png");
 
-	public String getIssueString() {
-		return issueString;
-	}
+        this.setHealthPointsVisible(false);
+        this.setHealthPointsImage("images" + File.separator + "herocard" + File.separator + "hpimage.png");
 
-	public void setIssueString(String issueString) {
-		this.issueString = issueString;
-	}
+        this.setBonusIconVisible(false);
+        this.setBonusIconImage("images" + File.separator + "herocard" + File.separator + "bonusIcon.png");
+        
+        nameColor = new Color(241, 157, 15);
+        classColor = new Color(241, 157, 15);
 
-	public boolean isHealthPointsVisible() {
-		return healthPointsVisible;
-	}
+        cardText = "Card Text";
 
-	public void setHealthPointsVisible(boolean healthPointsVisible) {
-		this.healthPointsVisible = healthPointsVisible;
-	}
+        quoteString1 = "\"Once in while you just";
+        quoteString2 = "gotta improvise!\"";
+        issueString = "- Citizen Snips, The Crab Cometh #4";
 
-	public String getHealthPointsImage() {
-		return healthPointsImage;
-	}
+        nameFontColor = Color.white;
+        nameFont = new Font("SF Ferretopia", Font.PLAIN, 50);
+        hpFontColor = Color.black;
+        hpFont = new Font("SF Ferretopia", Font.PLAIN, 70);
+        classFontColor = Color.black;
+        classFont = new Font("Comic Book", Font.PLAIN, 30);
+        descriptionFontColor = Color.black;
+        descriptionFont = new Font("Comic Book", Font.PLAIN, 24);
+        quoteFontColor = Color.black;
+        quoteFont = new Font("Comic Book", Font.PLAIN, 20);
+    }
 
-	public void setHealthPointsImage(String healthPointsImage) {
-		this.healthPointsImage = healthPointsImage;
-	}
+    public String getXML() {
+        String xml = "";
+        xml += " <herocard>\n";
+        xml += "  <id>" + getCardID() + "</id>\n";
+        xml += "  <name>" + getName() + "</name>\n";
+        xml += "  <classes>" + getClasses() + "</classes>\n";
+        xml += "  <healthpoints>" + getHealthPoints() + "</healthpoints>\n";
+        xml += "  <healthpointsvisible>" + isHealthPointsVisible() + "</healthpointsvisible>\n";
+        xml += "  <healthpointsimage>" + getHealthPointsImage() + "</healthpointsimage>\n";
+        xml += "  <portrait>" + getPortraitFile() + "</portrait>\n";
+        xml += "  <numberindeck>" + getNumberInDeck() + "</numberindeck>\n";
+        xml += "  <cardtext>" + getCardText() + "</cardtext>\n";
+        xml += "  <quotestring1>" + getQuoteString1() + "</quotestring1>\n";
+        xml += "  <quotestring2>" + getQuoteString2() + "</quotestring2>\n";
+        xml += "  <issuestring>" + getIssueString() + "</issuestring>\n";
+        xml += "  <classcolour>" + getClassColor().getRGB() + "</classcolour>\n";
+        xml += "  <namecolour>" + getNameColor().getRGB() + "</namecolour>\n";
 
-	public String getCardText() {
-		return cardText;
-	}
+        xml += "  <namefontcolor>" + getNameFontColor().getRGB() + "</namefontcolor>\n";
+        xml += "  <namefont>" + getNameFont().getFontName() + ";" + getNameFont().getStyle() + ";" + getNameFont().getSize() + "</namefont>\n";
+        xml += "  <hpfontcolor>" + getHpFontColor().getRGB() + "</hpfontcolor>\n";
+        xml += "  <hpfont>" + getHpFont().getFontName() + ";" + getHpFont().getStyle() + ";" + getHpFont().getSize() + "</hpfont>\n";
+        xml += "  <classfontcolor>" + getClassFontColor().getRGB() + "</classfontcolor>\n";
+        xml += "  <classfont>" + getClassFont().getFontName() + ";" + getClassFont().getStyle() + ";" + getClassFont().getSize() + "</classfont>\n";
+        xml += "  <descriptionfontcolor>" + getDescriptionFontColor().getRGB() + "</descriptionfontcolor>\n";
+        xml += "  <descriptionfont>" + getDescriptionFont().getFontName() + ";" + getDescriptionFont().getStyle() + ";" + getDescriptionFont().getSize() + "</descriptionfont>\n";
+        xml += "  <quotefontcolor>" + getQuoteFontColor().getRGB() + "</quotefontcolor>\n";
+        xml += "  <quotefont>" + getQuoteFont().getFontName() + ";" + getQuoteFont().getStyle() + ";" + getQuoteFont().getSize() + "</quotefont>\n";
 
-	public void setCardText(String cardText) {
-		this.cardText = cardText;
-	}
+        xml += " </herocard>\n";
+        return xml;
+    }
 
-	public Color getNameColor() {
-		return nameColor;
-	}
+    public String getQuoteString1() {
+        return quoteString1;
+    }
 
-	public void setNameColor(Color nameColor) {
-		this.nameColor = nameColor;
-	}
+    public void setQuoteString1(String quoteString1) {
+        this.quoteString1 = quoteString1;
+    }
 
-	public Color getClassColor() {
-		return classColor;
-	}
+    public String getQuoteString2() {
+        return quoteString2;
+    }
 
-	public void setClassColor(Color classColor) {
-		this.classColor = classColor;
-	}
+    public void setQuoteString2(String quoteString2) {
+        this.quoteString2 = quoteString2;
+    }
 
-	public Font getNameFont() {
-		return nameFont;
-	}
+    public String getIssueString() {
+        return issueString;
+    }
 
-	public void setNameFont(Font nameFont) {
-		this.nameFont = nameFont;
-	}
+    public void setIssueString(String issueString) {
+        this.issueString = issueString;
+    }
 
-	public Color getNameFontColor() {
-		return nameFontColor;
-	}
+    public boolean isHealthPointsVisible() {
+        return healthPointsVisible;
+    }
 
-	public void setNameFontColor(Color nameFontColor) {
-		this.nameFontColor = nameFontColor;
-	}
+    public void setHealthPointsVisible(boolean healthPointsVisible) {
+        this.healthPointsVisible = healthPointsVisible;
+    }
+    
+    public boolean isBonusIconVisible() {
+        return bonusIconVisible;
+    }
 
-	public Font getHpFont() {
-		return hpFont;
-	}
+    public void setBonusIconVisible(boolean bonusIconVisible) {
+        this.bonusIconVisible = bonusIconVisible;
+    }
 
-	public void setHpFont(Font hpFont) {
-		this.hpFont = hpFont;
-	}
+    public String getHealthPointsImage() {
+        return healthPointsImage;
+    }
 
-	public Color getHpFontColor() {
-		return hpFontColor;
-	}
+    public void setHealthPointsImage(String healthPointsImage) {
+        this.healthPointsImage = healthPointsImage;
+    }
 
-	public void setHpFontColor(Color hpFontColor) {
-		this.hpFontColor = hpFontColor;
-	}
+    public String getBonusIconImage() {
+        return bonusIconImage;
+    }
 
-	public Font getClassFont() {
-		return classFont;
-	}
+    public void setBonusIconImage(String bonusIconImage) {
+        this.bonusIconImage = bonusIconImage;
+    }
+    
+    public String getCardText() {
+        return cardText;
+    }
 
-	public void setClassFont(Font classFont) {
-		this.classFont = classFont;
-	}
+    public void setCardText(String cardText) {
+        this.cardText = cardText;
+    }
 
-	public Color getClassFontColor() {
-		return classFontColor;
-	}
+    public Color getNameColor() {
+        return nameColor;
+    }
 
-	public void setClassFontColor(Color classFontColor) {
-		this.classFontColor = classFontColor;
-	}
+    public void setNameColor(Color nameColor) {
+        this.nameColor = nameColor;
+    }
 
-	public Font getDescriptionFont() {
-		return descriptionFont;
-	}
+    public Color getClassColor() {
+        return classColor;
+    }
 
-	public void setDescriptionFont(Font descriptionFont) {
-		this.descriptionFont = descriptionFont;
-	}
+    public void setClassColor(Color classColor) {
+        this.classColor = classColor;
+    }
 
-	public Color getDescriptionFontColor() {
-		return descriptionFontColor;
-	}
+    public Font getNameFont() {
+        return nameFont;
+    }
 
-	public void setDescriptionFontColor(Color descriptionFontColor) {
-		this.descriptionFontColor = descriptionFontColor;
-	}
+    public void setNameFont(Font nameFont) {
+        this.nameFont = nameFont;
+    }
 
-	public Font getQuoteFont() {
-		return quoteFont;
-	}
+    public Color getNameFontColor() {
+        return nameFontColor;
+    }
 
-	public void setQuoteFont(Font quoteFont) {
-		this.quoteFont = quoteFont;
-	}
+    public void setNameFontColor(Color nameFontColor) {
+        this.nameFontColor = nameFontColor;
+    }
 
-	public Color getQuoteFontColor() {
-		return quoteFontColor;
-	}
+    public Font getHpFont() {
+        return hpFont;
+    }
 
-	public void setQuoteFontColor(Color quoteFontColor) {
-		this.quoteFontColor = quoteFontColor;
-	}
+    public void setHpFont(Font hpFont) {
+        this.hpFont = hpFont;
+    }
 
-	
+    public Color getHpFontColor() {
+        return hpFontColor;
+    }
+
+    public void setHpFontColor(Color hpFontColor) {
+        this.hpFontColor = hpFontColor;
+    }
+
+    public Font getClassFont() {
+        return classFont;
+    }
+
+    public void setClassFont(Font classFont) {
+        this.classFont = classFont;
+    }
+
+    public Color getClassFontColor() {
+        return classFontColor;
+    }
+
+    public void setClassFontColor(Color classFontColor) {
+        this.classFontColor = classFontColor;
+    }
+
+    public Font getDescriptionFont() {
+        return descriptionFont;
+    }
+
+    public void setDescriptionFont(Font descriptionFont) {
+        this.descriptionFont = descriptionFont;
+    }
+
+    public Color getDescriptionFontColor() {
+        return descriptionFontColor;
+    }
+
+    public void setDescriptionFontColor(Color descriptionFontColor) {
+        this.descriptionFontColor = descriptionFontColor;
+    }
+
+    public Font getQuoteFont() {
+        return quoteFont;
+    }
+
+    public void setQuoteFont(Font quoteFont) {
+        this.quoteFont = quoteFont;
+    }
+
+    public Color getQuoteFontColor() {
+        return quoteFontColor;
+    }
+
+    public void setQuoteFontColor(Color quoteFontColor) {
+        this.quoteFontColor = quoteFontColor;
+    }
 }
