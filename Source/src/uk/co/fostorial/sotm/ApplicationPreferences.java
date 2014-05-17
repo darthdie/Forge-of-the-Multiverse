0561d580026728b03c6016e8ef6bb1bc4fcaf9e6
darthdie <briarbear7@gmail.com>
2014-05-16 19:25:11 -0500

    Added ApplicationPreferences.java file for keeping track of Preferences.
    --CreatorFrame.java--
    The last used folder now persists between sessions.

---
+++ Source/src/uk/co/fostorial/sotm/ApplicationPreferences.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uk.co.fostorial.sotm;
+
+import java.io.File;
+import java.util.prefs.Preferences;
+
+/**
+ *
+ * @author MCP
+ */
+public class ApplicationPreferences {
+    private static final Preferences prefs;
+    
+    static {
+        prefs = Preferences.userRoot().node(ApplicationPreferences.class.getName());
+    }
+    
+    public static File getLastPath() {
+        return new File(prefs.get("lastPath", "."));
+    }
+    
+    public static void setLastPath(File path) {
+        if(path == null) {
+            return;
+        }
+        
+        prefs.put("lastPath", path.getAbsolutePath());
+    }
+}
--- Source/src/uk/co/fostorial/sotm/CreatorFrame.java
+++ Source/src/uk/co/fostorial/sotm/CreatorFrame.java
@@ -4,16 +4,16 @@ import java.awt.BorderLayout;
 import java.awt.Window;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowListener;
 import java.lang.reflect.Method;
-
+import javafx.stage.WindowEvent;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-
-
 import uk.co.fostorial.sotm.deck.DeckManager;
 import uk.co.fostorial.sotm.design.CreatorTab;
 import uk.co.fostorial.sotm.design.CreatorTabCardBack;
@@ -34,10 +34,10 @@ import uk.co.fostorial.sotm.structure.HeroFrontCard;
 import uk.co.fostorial.sotm.structure.VillainCard;
 import uk.co.fostorial.sotm.structure.VillainFrontCard;
 
-public class CreatorFrame extends JFrame implements ChangeListener {
-
+public class CreatorFrame extends JFrame implements ChangeListener, WindowListener {
+    
     private static final long serialVersionUID = 8105592648557148065L;
-
+    
     final static public int FILE_NEW_HERO_FRONT = 1;
     final static public int FILE_NEW_HERO_BACK = 2;
     final static public int FILE_NEW_HERO_CARD = 3;
@@ -49,38 +49,40 @@ public class CreatorFrame extends JFrame implements ChangeListener {
     final static public int FILE_NEW_VILLAIN_DECK = 9;
     final static public int FILE_NEW_ENVIRONMENT_DECK = 10;
     final static public int FILE_NEW_ENVIRONMENT_CARD = 11;
-
+    
     private CreatorMenuBar creatorMenuBar;
-
+    
     private JTabbedPane tabbedPane;
-
+    
     private JFileChooser chooser = new JFileChooser();
-
+    
     public CreatorFrame() {
         setupFrame();
         this.setVisible(true);
     }
-
+    
     private void setupFrame() {
+        addWindowListener(this);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500, 500);
         this.setTitle("Forge of the Multiverse");
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-
+        
         chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-
+        chooser.setCurrentDirectory(ApplicationPreferences.getLastPath());
+        
         creatorMenuBar = new CreatorMenuBar(this);
         this.setJMenuBar(creatorMenuBar);
-
+        
         tabbedPane = new JTabbedPane();
         tabbedPane.addChangeListener(this);
         tabbedPane.addMouseListener(new FrameMouseAdapter(this));
         this.add(tabbedPane, BorderLayout.CENTER);
-
+        
         enableOSXFullscreen(this);
         creatorMenuBar.noPaneSelected();
     }
-
+    
     public void newWindow(int type, Card card) {
         switch (type) {
             case FILE_NEW_HERO_FRONT:
@@ -131,12 +133,12 @@ public class CreatorFrame extends JFrame implements ChangeListener {
                 if (path.equals("")) {
                     break;
                 }
-
+                
                 Deck deck = DeckDocument.create(path).parse();
                 if (deck == null) {
                     break;
                 }
-
+                
                 if (deck.getType() == Deck.DeckType.Hero) {
                     tabbedPane.addTab(deck.getName(), new DeckManager(DeckManager.HERO_MODE, deck, this));
                 } else if (deck.getType() == Deck.DeckType.Villain) {
@@ -144,41 +146,41 @@ public class CreatorFrame extends JFrame implements ChangeListener {
                 } else if (deck.getType() == Deck.DeckType.Environment) {
                     tabbedPane.addTab(deck.getName(), new DeckManager(DeckManager.ENVIRONMENT_MODE, deck, this));
                 }
-
+                
                 tabbedPane.setSelectedIndex(tabbedPane.getComponentCount() - 1);
                 break;
         }
     }
-
+    
     private String browseForFile() {
         int outcome = chooser.showOpenDialog(this);
         if (outcome != JFileChooser.APPROVE_OPTION) {
             return "";
         }
-
+        
         return chooser.getSelectedFile().getAbsolutePath();
     }
-
+    
     public void closeCurrentFrame() {
         if (tabbedPane.getComponentCount() > 0) {
             tabbedPane.remove(tabbedPane.getSelectedIndex());
         }
     }
-
+    
     public void exportToJPEG() {
         if (tabbedPane.getComponentCount() > 0) {
             CreatorTab creatorTab = (CreatorTab) tabbedPane.getSelectedComponent();
             creatorTab.saveToJPG();
         }
     }
-
+    
     public void exportToPNG() {
         if (tabbedPane.getComponentCount() > 0) {
             CreatorTab creatorTab = (CreatorTab) tabbedPane.getSelectedComponent();
             creatorTab.saveToPNG();
         }
     }
-
+    
     @SuppressWarnings({"unchecked", "rawtypes"})
     public static void enableOSXFullscreen(Window window) {
         if (window != null) {
@@ -192,27 +194,27 @@ public class CreatorFrame extends JFrame implements ChangeListener {
             }
         }
     }
-
+    
     public CreatorMenuBar getCreatorMenuBar() {
         return creatorMenuBar;
     }
-
+    
     public void setCreatorMenuBar(CreatorMenuBar creatorMenuBar) {
         this.creatorMenuBar = creatorMenuBar;
     }
-
+    
     public JTabbedPane getTabbedPane() {
         return tabbedPane;
     }
-
+    
     public void setTabbedPane(JTabbedPane tabbedPane) {
         this.tabbedPane = tabbedPane;
     }
-
+    
     @Override
     public void stateChanged(ChangeEvent e) {
         JTabbedPane pane = (JTabbedPane) e.getSource();
-
+        
         if (pane.getSelectedIndex() == -1) {
             creatorMenuBar.noPaneSelected();
         } else if (pane.getSelectedComponent() instanceof DeckManager) {
@@ -224,37 +226,72 @@ public class CreatorFrame extends JFrame implements ChangeListener {
             creatorMenuBar.cardPaneSelected();
         }
     }
-
+    
     public JFileChooser getChooser() {
         return chooser;
     }
-
+    
     public void setChooser(JFileChooser chooser) {
         this.chooser = chooser;
     }
 
+    @Override
+    public void windowOpened(java.awt.event.WindowEvent e) {
+       
+    }
+
+    @Override
+    public void windowClosing(java.awt.event.WindowEvent e) {
+        ApplicationPreferences.setLastPath(chooser.getSelectedFile());
+    }
+
+    @Override
+    public void windowClosed(java.awt.event.WindowEvent e) {
+       
+    }
+
+    @Override
+    public void windowIconified(java.awt.event.WindowEvent e) {
+  
+    }
+
+    @Override
+    public void windowDeiconified(java.awt.event.WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(java.awt.event.WindowEvent e) {
+  
+    }
+
+    @Override
+    public void windowDeactivated(java.awt.event.WindowEvent e) {
+
+    }
+    
     public class FrameMouseAdapter extends MouseAdapter {
-
+        
         private final CreatorFrame frame;
-
+        
         public FrameMouseAdapter(CreatorFrame frame) {
             this.frame = frame;
         }
-
+        
         @Override
         public void mousePressed(MouseEvent e) {
             if (e.isPopupTrigger()) {
                 doPop(e);
             }
         }
-
+        
         @Override
         public void mouseReleased(MouseEvent e) {
             if (e.isPopupTrigger() && frame.getTabbedPane().getTabCount() > 0) {
                 doPop(e);
             }
         }
-
+        
         private void doPop(MouseEvent e) {
             frame.getCreatorMenuBar().getFrameMenu().show(e.getComponent(), e.getX(), e.getY());
         }
